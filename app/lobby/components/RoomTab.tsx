9bf561fc59074f63284c10f031a56754c6e0cca1
TiennDungg1606 <chudungfc@gmail.com>
2025-08-19 22:56:22 +0700

    ga

--- app/lobby/components/RoomTab.tsx
+++ app/lobby/components/RoomTab.tsx
@@ -82,6 +82,11 @@ export default function RoomTab({ roomInput, setRoomInput, handleCreateRoom, han
       setLoadingRooms(false);
     }, 3000);
 
+    // Interval để cập nhật danh sách phòng mỗi 3s (không ảnh hưởng đến loading state)
+    const interval = setInterval(() => {
+      if (!stopped) fetchRooms();
+    }, 3000);
+
     // Lắng nghe sự kiện update-active-rooms từ server để reload danh sách phòng ngay lập tức
     socket = io(API_BASE, { transports: ["websocket"] });
     socket.on("update-active-rooms", () => {
@@ -90,7 +95,8 @@ export default function RoomTab({ roomInput, setRoomInput, handleCreateRoom, han
 
     return () => {
       stopped = true;
-  clearTimeout(loadingTimer);
+      clearTimeout(loadingTimer);
+      clearInterval(interval);
       if (socket) socket.disconnect();
     };
   }, []);
