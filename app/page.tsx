594941665cd372dee43a8e7bf08e4d3f10df277e
TiennDungg1606 <chudungfc@gmail.com>
2025-11-25 09:33:21 +0700

    gf

--- app/page.tsx
+++ app/page.tsx
@@ -67,7 +67,7 @@ export default function HomePage() {
     <Head>
       <script
         async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8360769059197958"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8360769059197588"
         crossOrigin="anonymous"
       />
     </Head>
